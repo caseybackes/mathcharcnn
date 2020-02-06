41684a918f6354ac0150aec4ec657e88bb87d0ce
Casey Backes <casey@Caseys-MacBook-Pro.local>
2020-02-05 20:16:57 -0700

    modified create_training_data and train_model

--- src/create_training_data.py
+++ src/create_training_data.py
@@ -14,16 +14,17 @@ def create_training_data(data_directory, categories, shuffle=False, save_pickle=
     for category in categories:
         print(f"Building class '{category}'")
         path = os.path.join(data_directory, category)
-        print('Path to category:',path)
+        # print('Path to category:',path)
         class_num = categories.index(category) # index the label name to a number 
-        print('class num: ',class_num)
+        # print('class num: ',class_num)
         for img in os.listdir(path):
-            print(os.path.join(data_directory,img))
-            print("image to search for: ", img)
-            im_array = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE) 
-            new_array = cv2.resize(im_array, (45,45))
-            training_data.append([im_array, class_num])
-            #print(f'class: {category},img: ', img)
+            if img != '.DS_Store':
+                # print(os.path.join(data_directory,img))
+                # print("image to search for: ", img)
+                im_array = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE) 
+                new_array = cv2.resize(im_array, (45,45))
+                training_data.append([im_array, class_num])
+                #print(f'class: {category},img: ', img)
     print(f"Built {len(categories)} classes.")
 
     if shuffle:    
